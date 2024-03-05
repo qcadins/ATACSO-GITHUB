3afb2ec6e962ba38dad8827d54f6e11d2cddbc67
wiky.hendra <wiky.hendra@09-05-0045-0123.ad-ins.com>
2024-03-05 16:39:36 +0700

    ATEENDIGO - Fixing Codacy

--- Keywords/connection/UpdateTenantPartnerCredential.groovy
+++ Keywords/connection/UpdateTenantPartnerCredential.groovy
@@ -1,12 +1,10 @@
 package connection
 
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import java.sql.Connection
 import java.sql.ResultSet
 import java.sql.ResultSetMetaData
 import java.sql.Statement
 import com.kms.katalon.core.annotation.Keyword
-import internal.GlobalVariable
 
 class UpdateTenantPartnerCredential {
 
@@ -21,7 +19,7 @@ class UpdateTenantPartnerCredential {
 	getUpdateTenantPartnerCredentialStoreDB(Connection conn, String tenantCode) {
 		stm = conn.createStatement()
 
-		resultSet = stm.executeQuery("select mst.tenant_code, partner_code, encrypted_access_token, access_token_expired_date, TO_CHAR(mpt.dtm_crt, 'yyyy-mm-dd') from ms_partner_credential mpt left join ms_tenant mst on mpt.id_ms_tenant = mst.id_ms_tenant where mst.tenant_code = '"+tenantCode+"'")
+		resultSet = stm.executeQuery("select mst.tenant_code, partner_code, encrypted_access_token, access_token_expired_date, TO_CHAR(mpt.dtm_crt, 'yyyy-mm-dd') from ms_partner_credential mpt left join ms_tenant mst on mpt.id_ms_tenant = mst.id_ms_tenant where mst.tenant_code = '" + tenantCode + "'")
 		metadata = resultSet.metaData
 
 		columnCount = metadata.getColumnCount()
